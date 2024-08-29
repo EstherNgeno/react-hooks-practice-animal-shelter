e1937aeddfe21c7b0ff81d00d5e163c2d69cb074
EstherNgeno <esther.ngeno@student.moringaschool.com>
2024-08-29 22:25:35 +0300

    final commit

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -13,7 +13,7 @@ function Filters({onChangeType, onFindPetsClick}) {
     <div className="ui form">
       <h3>Animal type</h3>
       <div className="field">
-        <select name="type" id="type" aria-label="type">
+        <select onChange={handleTypeChange} name="type" id="type" aria-label="type">
           <option value="all">All</option>
           <option value="cat">Cats</option>
           <option value="dog">Dogs</option>
